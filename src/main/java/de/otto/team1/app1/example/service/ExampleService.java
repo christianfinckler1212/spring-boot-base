0b4d187ed0a713df6daa50e1722a7c71a5e348c4
Christian Finckler <christian.finckler@otto.de>
2018-04-09 17:51:25 +0200

    minis fix and jacoco works now with jdk10

--- src/main/java/de/otto/team1/app1/example/service/ExampleService.java
+++ src/main/java/de/otto/team1/app1/example/service/ExampleService.java
@@ -3,8 +3,11 @@ package de.otto.team1.app1.example.service;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import de.otto.team1.app1.example.domain.Example;
 
+@Service
 public class ExampleService {
 
   public List<Example> findAll() {
